fe07fd939f8a37aade3d6342c91ae5a74dd29930
Axom <adamowaissi22@gmail.com>
2026-08-22 12:56:05 +0300

    Update __init__.py

--- luaxune/__init__.py
+++ luaxune/__init__.py
@@ -1,7 +1,17 @@
-from .globals import *
-from .core import *
-from .types import *
-from .events import *
+from .runtime import LuauRuntime
+from .api import *
+from .datatypes import *
 from .instances import *
 from .services import *
+from .events import *
 from .enums import *
+
+def execute(code, env=None):
+    runtime = LuauRuntime()
+    return runtime.execute(code, env)
+
+def execute_file(path, env=None):
+    runtime = LuauRuntime()
+    return runtime.execute_file(path, env)
+
+__version__ = "1.0.0"
