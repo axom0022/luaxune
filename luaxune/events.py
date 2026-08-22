6f81d655c324c685941e9b43aac47a6c7fd0bcca
Axom <adamowaissi22@gmail.com>
2026-08-22 12:41:16 +0300

    Create events.py

---
+++ luaxune/events.py
@@ -0,0 +1,8 @@
+class _Event:
+    def __init__(self):
+        self._callbacks = []
+    def connect(self, callback):
+        self._callbacks.append(callback)
+        return lambda: self._callbacks.remove(callback)
+    def fire(self, *args):
+        for cb in self._callbacks: cb(*args)
