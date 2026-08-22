e1812c221494fcddc76b687a7f90fbe67c4ee0b3
Axom <adamowaissi22@gmail.com>
2026-08-22 13:39:31 +0300

    Update setup.py

--- luaxune/setup.py
+++ luaxune/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="luaxune",
-    version="0.1.0",
+    name='luaxune',
+    version='1.0.0',
     packages=find_packages(),
-    description="Full Luau/Roblox for Python",
-    author="",
-    license="MIT",
-    python_requires=">=3.7",
+    description='Full Luau runtime for mobile and desktop/PC (Python)',
+    author='',
+    license='MIT',
+    python_requires='>=3.7',
 )
