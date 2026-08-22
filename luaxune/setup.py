e42f975a658e186de88cf77879ce7308041996d9
Axom <adamowaissi22@gmail.com>
2026-08-22 12:43:28 +0300

    Create setup.py

---
+++ luaxune/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="luaxune",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Full Luau/Roblox for Python",
+    author="",
+    license="MIT",
+    python_requires=">=3.7",
+)
